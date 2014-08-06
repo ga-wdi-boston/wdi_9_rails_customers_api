99a79d7222451b6e7bbaa35be83430565d6da485
Tom Dyer <tdyer1@gmail.com>
2014-08-05 23:04:11 -0400

    Added CORS config

--- config/application.rb
+++ config/application.rb
@@ -26,17 +26,18 @@ module CustomersApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  end
 
-  # Add Rack::Cors as middleware
-  # WARNING: Allow ALL cross site scripting
-  config.middleware.use Rack::Cors do
-    allow do
-      # WARNING: Allow ALL cross site scripting from ALL domains
-      origins '*'
-      # WARNING: Allow ALL HTTP method
-      resource '*', :headers => :any, :methods => [:get, :post,:delete, :options]
+
+    # Add Rack::Cors as middleware
+    # WARNING: Allow ALL cross site scripting
+    config.middleware.use Rack::Cors do
+      allow do
+        # WARNING: Allow ALL cross site scripting from ALL domains
+        origins '*'
+        # WARNING: Allow ALL HTTP method
+        resource '*', :headers => :any, :methods => [:get, :post,:delete, :options]
+      end
     end
-  end
 
+  end
 end
